3f0ddc8227f8040b2d658c1388e8a8fa71b45380
Alpay Duymaz <72867675+alpayduymaz@users.noreply.github.com>
2022-09-18 16:39:47 +0300

    Add files via upload
    init

---
+++ MyLastProject/DataAccess/Concrete/EntityFramework/Context.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class Context:DbContext
+    {
+
+        public override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server:DESKTOP-H0M8STK\MSSQL;Database=MyLastDatabase;TrustedConnection=true");
+        }
+        public DbSet<Product> Products;
+    }
+}
---
+++ MyLastProject/Entities/Concrete/Product.cs
@@ -0,0 +1,13 @@
+﻿using CORE.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Product:IEntity
+    {
+    }
+}
