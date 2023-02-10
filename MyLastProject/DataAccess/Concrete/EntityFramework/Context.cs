4e959a852b523c9cb5308c59f6c7cddfdb0c2ad6
alpay duymaz <aduymaz@insoftpro.com>
2023-02-10 16:50:14 +0300

    Data Access Concrete

--- MyLastProject/DataAccess/Concrete/EntityFramework/Context.cs
+++ MyLastProject/DataAccess/Concrete/EntityFramework/Context.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Concrete;
+﻿using CORE.Entities.Concrete;
+using Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -15,6 +16,12 @@ namespace DataAccess.Concrete.EntityFramework
         {
             optionsBuilder.UseSqlServer(@"Server:DESKTOP-H0M8STK\MSSQL;Database=MyLastDatabase;TrustedConnection=true");
         }
-        public DbSet<Product> Products;
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OperationClaim> OperationClaims { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
     }
 }
