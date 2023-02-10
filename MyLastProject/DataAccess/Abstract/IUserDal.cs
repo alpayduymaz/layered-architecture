93689be88a223dbadd3b7659bfeae988876f1077
alpay duymaz <aduymaz@insoftpro.com>
2023-02-10 16:43:09 +0300

    add dal interfaces

---
+++ MyLastProject/DataAccess/Abstract/IUserDal.cs
@@ -0,0 +1,15 @@
+﻿using CORE.DataAccess;
+using CORE.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface IUserDal : IEntityRepository<User>
+    {
+        List<OperationClaim> GetClaims(User user);
+    }
+}
